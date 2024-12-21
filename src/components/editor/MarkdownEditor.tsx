e22df40254deea5d26bfd857d2c79e414cc90fc4
callsamu <samuelbree@gmail.com>
2024-12-21 11:02:58 -0300

    fix: scroll issue caused by new container sizing

--- src/components/editor/MarkdownEditor.tsx
+++ src/components/editor/MarkdownEditor.tsx
@@ -47,7 +47,7 @@ function MarkdownEditor({ initialContent, onSave, iconsSize = 18 }: MarkdownEdit
 
     return (
         <div className="w-full h-full flex flex-col min-h-96 bg-background text-foreground text-opacity-60">
-            <div data-cy="editor-menu" className="overflow-x-scroll w-full min-h-12 px-2 py-1 top-0 flex items-center border-b-border border-b gap-3 z-10 bg-background">
+            <div data-cy="editor-menu" className="overflow-x-scroll w-full min-h-12 px-2 py-1 top-0 sticky flex items-center border-b-border border-b gap-3 z-10 bg-background">
                 <NodeSelector editor={{
                     isActive: (node, attrs) => editor.isActive(node, attrs),
                     setNode: (node, attrs) => editor.chain().focus().setNode(node, attrs).run(),
